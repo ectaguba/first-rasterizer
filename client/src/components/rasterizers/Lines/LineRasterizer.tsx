9d46b7b580bf9e8697f62965b95d0a79cc74fe1f
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-01-30 15:07:59 -0500

    commit of working API request in LineRasterizer

--- client/src/components/rasterizers/Lines/LineRasterizer.tsx
+++ client/src/components/rasterizers/Lines/LineRasterizer.tsx
@@ -1,81 +1,102 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Canvas from "../Canvas.tsx";
 import LineField from "./LineField.tsx";
 import { Pixel } from "../Pixel.tsx";
 import { Line } from "../Line.tsx";
 import LineTest from './LineTest.tsx';
+import axios from "axios";
 
 const LineRasterizer: React.FC = () => {
 
     const canvasSize = { x: 640, y: 480 };
 
     // lines should draw a triangle
-    const [lineArr, setLineArr] = useState<Line[]>([
-        new Line(
-            new Pixel(0, 120, 1), 
-            new Pixel(-160, -120, 0)
-        ),
-        new Line(
-            new Pixel(-160, -120, 0), 
-            new Pixel(160, -120, 0.5)
-        ),
-        new Line(
-            new Pixel(160, -120, 0.5), 
-            new Pixel(0, 120, 1)
-        )
-    ]);
+    // const [lineArr, setLineArr] = useState<Line[]>([
+    //     new Line(
+    //         new Pixel(0, 120, 1), 
+    //         new Pixel(-160, -120, 0)
+    //     ),
+    //     new Line(
+    //         new Pixel(-160, -120, 0), 
+    //         new Pixel(160, -120, 0.5)
+    //     ),
+    //     new Line(
+    //         new Pixel(160, -120, 0.5), 
+    //         new Pixel(0, 120, 1)
+    //     )
+    // ]);
+    
+    let lineJSON = {};
+    const [lineArr, setLineArr] = useState({});
 
-    const handleChangeLine = (
-        index: number, 
-        point: 0 | 1, 
-        property: 'x' | 'y' | 'h' | 'color', 
-        value: string | number
-    ): void => {
-        const updatedLines: Line[] = [...lineArr];
-        updatedLines.map((line, i) => {
-            if (i === index) {
-                line.updatePoint(point, property, value);
-            }
-        });
-        setLineArr(updatedLines);
-    };
+    useEffect(() => {
+        axios
+            .get("http://localhost:8082/api/canvasElements")
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log("Error from LineRasterizer");
+            })
+    }, [])
 
-    const handleAddLine = (): void => {
-        // initialize new pixel and line objects
-        const newLine = new Line(new Pixel(0, 0), new Pixel(0, 0));
-        // insert into existing array
-        setLineArr((prevLineArr) => [...prevLineArr, newLine]);
-    }
+    // const handleChangeLine = (
+    //     index: number, 
+    //     point: 0 | 1, 
+    //     property: 'x' | 'y' | 'h' | 'color', 
+    //     value: string | number
+    // ): void => {
+    //     const updatedLines: Line[] = [...lineArr];
+    //     updatedLines.map((line, i) => {
+    //         if (i === index) {
+    //             line.updatePoint(point, property, value);
+    //         }
+    //     });
+    //     setLineArr(updatedLines);
+    // };
 
+    // const handleAddLine = (): void => {
+    //     // initialize new pixel and line objects
+    //     const newLine = new Line(new Pixel(0, 0), new Pixel(0, 0));
+    //     // insert into existing array
+    //     setLineArr((prevLineArr) => [...prevLineArr, newLine]);
+    // }
+
+    // return (
+    //     <div className="LineRasterizer">
+    //         <div className="CanvasContainer">
+    //             <Canvas
+    //                 modelArr={lineArr}
+    //                 width={canvasSize.x}
+    //                 height={canvasSize.y}
+    //             />
+    //         </div>
+    //         <div className="LineFieldContainer">
+    //             {lineArr.map((item: Line, index: number) => (
+    //                 <LineField
+    //                     key={index}
+    //                     lineIndex={index}
+    //                     line={item}
+    //                     handleChangeLine={handleChangeLine}
+    //                 />
+    //             ))}
+    //             <button 
+    //                 className="AddLineBtn"
+    //                 type="button" 
+    //                 onClick={handleAddLine}
+    //             >
+    //                 Add Line
+    //             </button>
+    //         </div>
+    //     </div>
+    // );
     return (
-        <div className="LineRasterizer">
-            <div className="CanvasContainer">
-                <Canvas
-                    modelArr={lineArr}
-                    width={canvasSize.x}
-                    height={canvasSize.y}
-                />
-            </div>
-            <div className="LineFieldContainer">
-                {lineArr.map((item: Line, index: number) => (
-                    <LineField
-                        key={index}
-                        lineIndex={index}
-                        line={item}
-                        handleChangeLine={handleChangeLine}
-                    />
-                ))}
-                <button 
-                    className="AddLineBtn"
-                    type="button" 
-                    onClick={handleAddLine}
-                >
-                    Add Line
-                </button>
-            </div>
+        <div>
+            <p>Line rasterizer:</p>
+            {JSON.stringify(lineJSON)}
         </div>
-    );
+    )
 };
 
 export default LineRasterizer;
