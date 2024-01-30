36ddf2b639b06c59f329714ec0c57036c48111df
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-01-30 15:08:35 -0500

    created test object of CanvasElement schema

--- server/models/CanvasElement.js
+++ server/models/CanvasElement.js
@@ -20,6 +20,24 @@ const pixelSchema = new Schema({
     },
 });
 
+const colorSchema = new Schema({
+    r: {
+        type: Number,
+        required: true,
+        default: 128
+    },
+    g: { 
+        type: Number, 
+        required: true, 
+        default: 128 
+    },
+    b: { 
+        type: Number, 
+        required: true,
+        default: 128 
+    },
+});
+
 const canvasElementSchema = new mongoose.Schema({
     type: { 
         type: String, 
@@ -31,7 +49,7 @@ const canvasElementSchema = new mongoose.Schema({
         required: true,
     },
     color: {
-        type: [Number],
+        type: [colorSchema],
         required: true,
         default: [0, 255, 0] // green
     },
@@ -47,3 +65,26 @@ const canvasElementSchema = new mongoose.Schema({
 const CanvasElement = mongoose.model('CanvasElement', canvasElementSchema);
 
 module.exports = CanvasElement;
+
+// const test = new CanvasElement
+// (
+//     {
+//         "_id": {
+//           "$oid": "65b95294a64f38e7e562fa98"
+//         },
+//         "type": "line",
+//         "vertices": [
+//           {
+//             "x": 0,
+//             "y": 120,
+//             "h": 0.5
+//           },
+//           {
+//             "x": -160,
+//             "y": -120,
+//             "h": 1
+//           }
+//         ],
+//         "color": [255, 0, 0]
+//       }
+// )
