c4d606db62443f595c79ca61c56c685432bcc164
ectaguba <113264419+niichirin@users.noreply.github.com>
2024-01-30 15:07:19 -0500

    changed react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,27 +11,16 @@ import {
 import HomePage from './components/HomePage.tsx';
 import LineRasterizer from './components/rasterizers/Lines/LineRasterizer.tsx';
 
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/line-rasterizer", element: <LineRasterizer/> },
+]);
+
 const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/lines">Line Rasterizer</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/lines" element={<LineRasterizer />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
-    </Router>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 };
 
